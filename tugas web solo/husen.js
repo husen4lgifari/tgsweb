95308c9a0c8466c076bfe04b7f7c6a7114c71e2c
Husen Algifari <139084166+husen4lgifari@users.noreply.github.com>
2023-07-20 01:05:03 +0100

    Add files via upload

---
+++ tugas web solo/husen.js
@@ -0,0 +1,198 @@
+
+                function shareFacebook1(link) {
+                  var encodedLink = encodeURIComponent(link);
+                  var url = "https://www.facebook.com/sharer/sharer.php?u=" + encodedLink;
+                  window.open(url, "_blank");
+                  $('#shareModal1').modal('hide');
+                }
+
+                function shareTwitter1(link) {
+                  var encodedLink = encodeURIComponent(link);
+                  var url = "https://twitter.com/intent/tweet?url=" + encodedLink;
+                  window.open(url, "_blank");
+                  $('#shareModal1').modal('hide');
+                }
+
+                function shareEmail1(subject, body) {
+                  var encodedSubject = encodeURIComponent(subject);
+                  var encodedBody = encodeURIComponent(body);
+                  var url = "mailto:?subject=" + encodedSubject + "&body=" + encodedBody;
+                  window.location.href = url;
+                  $('#shareModal1').modal('hide');
+                }
+                function shareFacebook2(link) {
+                    var encodedLink = encodeURIComponent(link);
+                    var url = "https://www.facebook.com/sharer/sharer.php?u=" + encodedLink;
+                    window.open(url, "_blank");
+                    $('#shareModal2').modal('hide');
+                  }
+  
+                  function shareTwitter2(link) {
+                    var encodedLink = encodeURIComponent(link);
+                    var url = "https://twitter.com/intent/tweet?url=" + encodedLink;
+                    window.open(url, "_blank");
+                    $('#shareModal2').modal('hide');
+                  }
+  
+                  function shareEmail2(subject, body) {
+                    var encodedSubject = encodeURIComponent(subject);
+                    var encodedBody = encodeURIComponent(body);
+                    var url = "mailto:?subject=" + encodedSubject + "&body=" + encodedBody;
+                    window.location.href = url;
+                    $('#shareModal2').modal('hide');
+                  }
+                  function shareFacebook3() {
+                    var link = encodeURIComponent("https://shp.ee/rzqf6h5");
+                    var url = "https://www.facebook.com/sharer/sharer.php?u=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareTwitter3() {
+                    var link = encodeURIComponent("https://shp.ee/rzqf6h5");
+                    var url = "https://twitter.com/intent/tweet?url=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareEmail3() {
+                    var subject = encodeURIComponent("Check out this video");
+                    var body = encodeURIComponent("I found this interesting video: https://shp.ee/rzqf6h5");
+                    var url = "mailto:?subject=" + subject + "&body=" + body;
+                    window.location.href = url;
+                    $('#shareModal').modal('hide');
+                  }
+                  function shareFacebook4() {
+                    var link = encodeURIComponent("https://shp.ee/zxrs6jj");
+                    var url = "https://www.facebook.com/sharer/sharer.php?u=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareTwitter4() {
+                    var link = encodeURIComponent("https://shp.ee/zxrs6jj");
+                    var url = "https://twitter.com/intent/tweet?url=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareEmail4() {
+                    var subject = encodeURIComponent("Check out this video");
+                    var body = encodeURIComponent("I found this interesting video: https://shp.ee/zxrs6jj");
+                    var url = "mailto:?subject=" + subject + "&body=" + body;
+                    window.location.href = url;
+                    $('#shareModal').modal('hide');
+                  }
+                  function shareFacebook6() {
+                    var link = encodeURIComponent("https://shp.ee/z7hpxrk");
+                    var url = "https://www.facebook.com/sharer/sharer.php?u=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareTwitter6() {
+                    var link = encodeURIComponent("https://shp.ee/z7hpxrk");
+                    var url = "https://twitter.com/intent/tweet?url=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareEmail6() {
+                    var subject = encodeURIComponent("Check out this video");
+                    var body = encodeURIComponent("I found this interesting video: https://shp.ee/z7hpxrk");
+                    var url = "mailto:?subject=" + subject + "&body=" + body;
+                    window.location.href = url;
+                    $('#shareModal').modal('hide');
+                  }
+                  function shareFacebook5() {
+                    var link = encodeURIComponent("https://shp.ee/qqwnipp");
+                    var url = "https://www.facebook.com/sharer/sharer.php?u=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareTwitter5() {
+                    var link = encodeURIComponent("https://shp.ee/qqwnipp");
+                    var url = "https://twitter.com/intent/tweet?url=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareEmail5() {
+                    var subject = encodeURIComponent("Check out this video");
+                    var body = encodeURIComponent("I found this interesting video: https://shp.ee/qqwnipp");
+                    var url = "mailto:?subject=" + subject + "&body=" + body;
+                    window.location.href = url;
+                    $('#shareModal').modal('hide');
+                  }
+                  function shareFacebook7() {
+                    var link = encodeURIComponent("https://shp.ee/3p3je83");
+                    var url = "https://www.facebook.com/sharer/sharer.php?u=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareTwitter7() {
+                    var link = encodeURIComponent("https://shp.ee/3p3je83");
+                    var url = "https://twitter.com/intent/tweet?url=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareEmail7() {
+                    var subject = encodeURIComponent("Check out this video");
+                    var body = encodeURIComponent("I found this interesting video: https://shp.ee/3p3je83");
+                    var url = "mailto:?subject=" + subject + "&body=" + body;
+                    window.location.href = url;
+                    $('#shareModal').modal('hide');
+                  }
+                  function shareFacebook8() {
+                    var link = encodeURIComponent("https://shp.ee/5sy3erq");
+                    var url = "https://www.facebook.com/sharer/sharer.php?u=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareTwitter8() {
+                    var link = encodeURIComponent("https://shp.ee/5sy3erq");
+                    var url = "https://twitter.com/intent/tweet?url=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareEmail8() {
+                    var subject = encodeURIComponent("Check out this video");
+                    var body = encodeURIComponent("I found this interesting video: https://shp.ee/5sy3erq");
+                    var url = "mailto:?subject=" + subject + "&body=" + body;
+                    window.location.href = url;
+                    $('#shareModal').modal('hide');
+                  }
+                  function shareFacebook() {
+                    var link = encodeURIComponent("https://shp.ee/kiqg2y4");
+                    var url = "https://www.facebook.com/sharer/sharer.php?u=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareTwitter() {
+                    var link = encodeURIComponent("https://shp.ee/kiqg2y4");
+                    var url = "https://twitter.com/intent/tweet?url=" + link;
+                    window.open(url, "_blank");
+                    $('#shareModal').modal('hide');
+                  }
+  
+                  function shareEmail() {
+                    var subject = encodeURIComponent("Check out this video");
+                    var body = encodeURIComponent("I found this interesting video: https://shp.ee/kiqg2y4");
+                    var url = "mailto:?subject=" + subject + "&body=" + body;
+                    window.location.href = url;
+                    $('#shareModal').modal('hide');
+                  }
+                  function sendWhatsAppMessage() {
+                    var name = document.getElementById('name').value;
+                    var message = document.getElementById('message').value;
+              
+                    var whatsappURL = 'https://api.whatsapp.com/send?phone=6283129055606&text=' + encodeURIComponent('Nama: ' + name + '\n' + 'Pesan: ' + message);
+              
+                    window.open(whatsappURL, '_blank');
+                  }
